d3508ecee0f6f9712a874fa591a2a03f7a72196b
lesego16 <49120340+lesego16@users.noreply.github.com>
2025-04-15 09:19:08 +0000

    printed a hello world text

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,14 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World" << endl;
+    return 0;
+}
